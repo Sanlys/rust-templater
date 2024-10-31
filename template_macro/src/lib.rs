1c508ba2069d8f1c444fac9547590e3f15d579cf
Sander Lysakermoen <sander@lysakermoen.com>
2024-10-31 21:48:29 +0100

    Jeg kommer til å henge meg selv

--- template_macro/src/lib.rs
+++ template_macro/src/lib.rs
@@ -2,11 +2,31 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
 use regex::Regex;
-use syn::Ident;
+use syn::{parse_macro_input, Ident, LitStr};
+use std::fs;
+use std::env::current_dir;
 
 #[proc_macro]
 pub fn template_macro(input: TokenStream) -> TokenStream {
-    let input_tokens = input.to_string();
+    let raw_filename = parse_macro_input!(input as LitStr);
+    let filename = raw_filename.value();
+    let file_contents = match fs::read_to_string(&filename) {
+        Ok(val) => {
+            val
+        },
+        _ => {
+            match current_dir() {
+                Ok(dir) => {
+                    dir.to_string_lossy().to_string()
+                },
+                _ => {
+                    "kill me".to_string()
+                }
+            }
+        }
+    };
+
+    let input_tokens = file_contents;
 
     let re = Regex::new(r"\{\ *\{(\s*[^}]+\s*)\} *\}").unwrap();
     let formatted = re.replace_all(&input_tokens, "{}");
